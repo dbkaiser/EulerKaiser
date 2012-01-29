0e15f2224305590ac0b02471c8be46bad57ded9d
edogboo <douglas.dong@ericsson.com>
2012-01-29 17:33:43 +0800

    add 31

---
+++ src/com/dbk/puzzle/Puzzle31.java
@@ -0,0 +1,21 @@
+package com.dbk.puzzle;
+
+import java.util.Stack;
+
+/**
+ * 
+ * @author edogboo
+ *
+ */
+public class Puzzle31 {
+
+	public static int routeCount = 0;
+	public static int[] rock = {200,100,50,20,10,5,2,1};
+	public static Stack<Integer> stack = new Stack<Integer>(); 
+	
+	public static void main(String args[]){
+		
+	}
+	
+	
+}
