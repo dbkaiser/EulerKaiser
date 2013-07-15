b8f2b1152cae0b40fdf9dfec845f439ab321cc50
dbkaiser <dbkaiser@gmail.com>
2013-07-15 23:18:24 +0800

    purify the methods

--- python/pb41.py
+++ python/pb41.py
@@ -2,20 +2,6 @@
 
 from itertools import permutations
 import math
-primes=[];
-
-def storePrime(bound):
-    """store all the primes"""
-    primes=[0]*(bound)
-    times=int(math.sqrt(bound))+1
-    for i in range(2, times+1):
-        q = int(bound / times)+1
-        for j in range(2, q +1):
-            if i*j >= bound: 
-                continue;
-            primes[i*j]=1;
-
-    return primes
 
 def listToDigit(lists):
     return int(''.join(str(u) for u in lists))
@@ -28,11 +14,12 @@ def isPrime(num):
     root=int(math.ceil(math.sqrt(num+1)))
     print root
     for i in range(3,root+1, 2):
-        print i,"wehi"
         if num%i==0 : return False
     return True
 
-lists=permutations(range(9,0,-1))
+# it is surely that 1-8 digits will be divided by 3, and so do 1-9
+lists=permutations(range(7,0,-1))
+
 for num in lists:
     thedi=listToDigit(num)
     if isPrime(thedi): 
@@ -42,6 +29,7 @@ for num in lists:
         break
 
     
+print "done"
 
 
 
